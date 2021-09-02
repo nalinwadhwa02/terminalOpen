859d32338236b96abdae8d33db160754db03b650
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2021-09-02 19:08:57 +0530

    changes2

---
+++ testing/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "../include/fileClass.hpp"
+int main(int argc, char * argv[]){
+//on start
+//	>get directory
+//	>clear copy/paste buffer
+//work
+//	>find all files in directory
+//	>add funtionality to move about/edit the files
+//	>update directory once entered.
+	return 0;
+}
+
